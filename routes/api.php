0a32d5b7b10590ed12df1eb11981d17bc5cf54c1
BhushanChowta <bhushanchowta@gmail.com>
2024-09-15 16:31:38 +0530

    api.php route update

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\DiscountController;
 
-Route::get('/applyDiscount', [DiscountController::class, 'applyDiscount']);
+Route::post('/applyDiscount', [DiscountController::class, 'applyDiscount']);
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
